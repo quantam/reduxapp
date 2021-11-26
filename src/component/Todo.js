18334c447a7a75e9fdfb0b738a43db984b82d93c
Santu Rana <santurana.89@gmail.com>
2021-11-26 17:38:09 +0530

    css changes

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -9,15 +9,20 @@ function Todo() {
   return (
     <div className="parent-div">
       <div className="child-div">
-        <figure>
-          <figcaption>Add your list here</figcaption>
-        </figure>
+        <h2>Add your list here : TODO</h2>
 
         <div className="add-item">
           <input type="text" placeholder="Add text here...." value={inputData} onChange={(event) => setInputData(event.target.value)} />
           <button className="btn" onClick={() => dispatch(addTodo(inputData))}>
             +
           </button>
+        </div>
+
+        <div className="display-item">
+          <ul>
+            <li>Test</li>
+            <li>Test</li>
+          </ul>
         </div>
       </div>
     </div>
